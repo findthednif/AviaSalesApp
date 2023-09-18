14a51f02827b3c1821fd0a73f71b00ce38912bec
Vladislav Kazakov <vladislavkazakov998@gmail.com>
2023-09-19 02:28:27 +0500

    mentors changes

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -7,51 +7,38 @@ import { toggleAll, toggleCheckbox } from '../Redux/actions';
 export default function Filters() {
   const checkboxState = useSelector((state) => state.checkboxReducer);
   const dispatch = useDispatch();
+  const checkboxes = [
+    { id: 'all', label: 'Все' },
+    { id: 'withoutTransfers', label: 'Без пересадок' },
+    { id: 'oneTransfer', label: '1 пересадка' },
+    { id: 'twoTransfers', label: '2 пересадки' },
+    { id: 'threeTransfers', label: '3 пересадки' },
+  ];
+
   return (
     <div className='filters'>
       <h1 className='filters__title'>КОЛЛИЧЕСТВО ПЕРЕСАДОК</h1>
-      <label>
-        <input
-          type='checkbox'
-          checked={checkboxState.all}
-          onChange={() => dispatch(toggleAll())}
-        />
-        <span className='checkmark' />
-        Все
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          checked={checkboxState.withoutTransfers}
-          onChange={() => dispatch(toggleCheckbox('withoutTransfers'))}
-        />
-        <span className='checkmark' />
-        Без пересадок
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          checked={checkboxState.oneTransfer}
-          onChange={() => dispatch(toggleCheckbox('oneTransfer'))}
-        />
-        <span className='checkmark' />1 пересадка
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          checked={checkboxState.twoTransfers}
-          onChange={() => dispatch(toggleCheckbox('twoTransfers'))}
-        />
-        <span className='checkmark' />2 пересадки
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          checked={checkboxState.threeTransfers}
-          onChange={() => dispatch(toggleCheckbox('threeTransfers'))}
-        />
-        <span className='checkmark' />3 пересадки
-      </label>
+      {checkboxes.map((checkbox) => (
+        <label key={checkbox.id}>
+          <input
+            type='checkbox'
+            checked={
+              checkbox.id === 'all'
+                ? checkboxState.all
+                : checkboxState[checkbox.id]
+            }
+            onChange={() => {
+              if (checkbox.id === 'all') {
+                dispatch(toggleAll());
+              } else {
+                dispatch(toggleCheckbox(checkbox.id));
+              }
+            }}
+          />
+          <span className='checkmark' />
+          {checkbox.label}
+        </label>
+      ))}
     </div>
   );
 }
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,40 +5,28 @@ import { setActiveTab } from '../Redux/actions';
 import './Tabs.scss';
 
 export default function Tabs() {
+  const tabs = [
+    { id: 0, label: 'САМЫЙ ДЕШЕВЫЙ' },
+    { id: 1, label: 'САМЫЙ БЫСТРЫЙ' },
+    { id: 2, label: 'ОПТИМАЛЬНЫЙ' },
+  ];
+
   const activeTab = useSelector((state) => state.tabsReducer.activeTab);
   const dispatch = useDispatch();
 
   return (
     <ul className='tabs'>
-      <li className='tabs__tab'>
-        <button
-          type='button'
-          onClick={() => dispatch(setActiveTab(0))}
-          className={`tabs__button ${activeTab === 0 ? 'active' : ''}`}
-        >
-          САМЫЙ ДЕШЕВЫЙ
-        </button>
-      </li>
-      <li className='tabs__tab'>
-        <button
-          type='button'
-          onClick={() => dispatch(setActiveTab(1))}
-          className={`tabs__button ${activeTab === 1 ? 'active' : ''}`}
-        >
-          САМЫЙ БЫСТРЫЙ
-        </button>
-      </li>
-      <li className='tabs__tab'>
-        <button
-          type='button'
-          onClick={() => {
-            dispatch(setActiveTab(2));
-          }}
-          className={`tabs__button ${activeTab === 2 ? 'active' : ''}`}
-        >
-          ОПТИМАЛЬНЫЙ
-        </button>
-      </li>
+      {tabs.map((tab) => (
+        <li key={tab.id} className='tabs__tab'>
+          <button
+            type='button'
+            onClick={() => dispatch(setActiveTab(tab.id))}
+            className={`tabs__button ${activeTab === tab.id ? 'active' : ''}`}
+          >
+            {tab.label}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
